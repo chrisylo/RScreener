045151a247f92a585e690f52187b36a928dd8fef
chrisylo <55366234+chrisylo@users.noreply.github.com>
2024-04-11 12:23:08 +1000

    var name correction

--- main.py
+++ main.py
@@ -179,15 +179,6 @@ if submit :
     cand2_score_5 = response.content    
 
     ## Candidate 3 PDF
-    # process PDF
-    temp_dir = tempfile.mkdtemp(dir=os.getcwd())
-    save_path_cv = Path(temp_dir, can3_file.name)
-    with open(save_path_cv, mode='wb') as w:
-         w.write(can3_file.getvalue())
-    
-    cand3_file_path = os.path.join(temp_dir, cand3_file.name)
-    cand3 = extract_text_from_pdf(cand3_file_path)
-    
     recruit_chat_history.add_user_message(f'this is the profile of candidate 3: {cand3}. What is the name of this candidate? In your response, include his/her name only (no other words or punctuation)')
     response = chain.invoke({"messages": recruit_chat_history.messages})
     recruit_chat_history.add_ai_message(response)
